498560812781c021b2bdda0de5b5e48e93ce8539
rusov63 <rusov63@yahoo.com>
2023-02-19 21:20:32 +0400

    созданы тесты, и поправлен основной код

--- utils/task.py
+++ utils/task.py
@@ -2,22 +2,29 @@ class Store:
     discount = 0.9
 
     def __init__(self, product_name, unit_price, quantity):
+        """Название товара, цена за единицу и количество товара в магазине"""
         self.product_name = product_name
         self.unit_price = unit_price
         self.quantity = quantity
 
+    def total_cost(self):
+        """Общая стоимость товара"""
+        return self.quantity * self.unit_price
+
     def price_discount(self):
-        self.unit_price = self.unit_price * self.discount
+        """Скидка для товара"""
+        return self.unit_price * self.discount
 
 
-store = Store('laptop_asus', 30000, 6)
+store = Store('laptop_asus', 30_000, 6)
 print(store.product_name)
-print(store.unit_price)
+store.total_cost()
+print(f'Общая стоимость товара: {store.total_cost()}')
 store.price_discount()
-print(store.unit_price)
+print(f'Стоимость товара с учетом скидки: {store.unit_price}')
 
-store1 = Store('tablet_lenovo', 15000, 10)
+store1 = Store('tablet_lenovo', 15_000, 10)
 print(store1.product_name)
-print(store1.unit_price)
+print(f'Общая стоимость товара: {store1.total_cost()}')
 store1.price_discount()
-print(store1.unit_price)
+print(f'Стоимость товара с учетом скидки: {store1.unit_price}')

8abbe576c5363d3bbd1d6b76b2ce5f52321e6f77
rusov63 <rusov63@yahoo.com>
2023-03-01 17:05:50 +0400

    Домашние задание №3 14.1

--- tests/test_task.py
+++ tests/test_task.py
@@ -8,3 +8,11 @@ def test_is_integer():
     assert Item.is_integer(5.0) == True
     assert Item.is_integer(5.5) == False
 
+def test__repr__():
+    item = Item("Ноутбук", 20000, 5)
+    assert item.__repr__() != Item('Смартфон', '10000', '20')
+
+def test__str__():
+    item = Item("Ноутбук", 20000, 5)
+    assert item.__str__() != Item('Смартфон', '10000', '20')
+
